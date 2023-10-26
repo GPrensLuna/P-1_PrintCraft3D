10a200e66fd9010ea27105f8f7127d591693aa1b
GPrensLuna <prens.giova12@gmail.com>
2023-10-25 21:36:41 -0500

    :white_check_mark: INICIO   :tada:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-// import Home from "./views/Home/Home.jsx";
+ import Home from "./views/Home/Home.jsx";
 import { Login, Inventory } from "./views";
 import NavBar from "./Components/NavBar/NavBar.jsx";
- //<Route path="/" element={<Home />}></Route>
-// ruta en conflicto trabajando en solucion 
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-       <Routes>
-        
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+
         <Route path="/Registrarse" element={<Login />}></Route>
         <Route path="/Inventario" element={<Inventory />}></Route>
       </Routes>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -8,17 +8,11 @@ import Aside from "../../Components/Aside/Aside.jsx";
 import imagenes from "../../imagenes/images.js";
 import style from "./Home.module.css";
 import Card from "../../Components/Card/Card.jsx";
-import { addProductInfo, deleteProduct } from "../../redux/actions/actions.js";
+import { addProductInfo } from "../../redux/actions/actions.js";
 
 function Home() {
   const dispatch = useDispatch();
   const allProducts = useSelector((state) => state.allProducts);
-  const deletedCardsFromLocalStorage =
-    JSON.parse(localStorage.getItem("deletedCards")) || [];
-
-  const [deletedCards, setDeletedCards] = useState(
-    deletedCardsFromLocalStorage
-  );
 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -31,36 +25,32 @@ function Home() {
   const [count, setCount] = useState(0);
   const [limit, setLimit] = useState(pokemonPerPage);
 
-  const [priceFilter, setPriceFilter] = useState(null);
-  const [sizeFilter, setSizeFilter] = useState(null);
-  const [materialFilter, setMaterialFilter] = useState("");
-  const [categoryFilter, setCategoryFilter] = useState("");
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         `${URL}Inventario?page=${currentPage}&limit=${pokemonPerPage}`
-  //       );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${URL}Inventario?page=${currentPage}&limit=${pokemonPerPage}`
+        );
 
-  //       if (response.status === 200) {
-  //         const { data } = response;
-  //         dispatch(addProductInfo(data.results));
+        if (response.status === 200) {
+          const { data } = response;
+          dispatch(addProductInfo(data.results));
 
-  //         setCount(data.count);
-  //         setLimit(data.limit);
-  //         setLoading(false);
-  //       } else {
-  //         setError("No se pudieron cargar los productos.");
-  //         setLoading(false);
-  //       }
-  //     } catch (error) {
-  //       setError("Hubo un error al recuperar los productos.");
-  //       setLoading(false);
-  //     }
-  //   };
+          setCount(data.count);
+          setLimit(data.limit);
+          setLoading(false);
+        } else {
+          setError("No se pudieron cargar los productos.");
+          setLoading(false);
+        }
+      } catch (error) {
+        setError("Hubo un error al recuperar los productos.");
+        setLoading(false);
+      }
+    };
 
-  //   fetchData();
-  // }, [currentPage, dispatch]);
+    fetchData();
+  }, [currentPage, dispatch]);
 
   useEffect(() => {
     localStorage.setItem("currentPage", currentPage);
@@ -74,35 +64,6 @@ function Home() {
     }
     setCurrentPage(page);
   };
-
-  const handleDeleteCard = (name) => {
-    // Actualiza el estado local de tarjetas eliminadas
-    const updatedDeletedCards = [...deletedCards, name];
-    setDeletedCards(updatedDeletedCards);
-    // Actualiza las tarjetas eliminadas en LocalStorage
-    localStorage.setItem("deletedCards", JSON.stringify(updatedDeletedCards));
-
-    // Supongamos que tienes una acción 'deleteProduct' que elimina un producto en Redux.
-    dispatch(deleteProduct(name));
-  };
-
-  const filteredProducts = allProducts
-    .filter((e) => !deletedCards.includes(e.name))
-    .filter((e) => {
-      if (priceFilter && e.price > Number(priceFilter)) {
-        return false;
-      }
-      if (sizeFilter && e.size !== sizeFilter) {
-        return false;
-      }
-      if (materialFilter && e.Material.name !== materialFilter) {
-        return false;
-      }
-      if (categoryFilter && e.Category.name !== categoryFilter) {
-        return false;
-      }
-      return true;
-    });
 
   return (
     <main className={style.main}>
@@ -134,88 +95,6 @@ function Home() {
             >
               Siguiente
             </button>
-            <div className={style.FilterInputs}>
-              <input
-                type="number"
-                placeholder="Precio máximo"
-                value={priceFilter || ""}
-                onChange={(e) =>
-                  setPriceFilter(
-                    e.target.value === "" ? null : parseFloat(e.target.value)
-                  )
-                }
-              />
-              <select
-                value={sizeFilter || ""}
-                onChange={(e) =>
-                  setSizeFilter(e.target.value === "" ? null : e.target.value)
-                }
-              >
-                <option value="">Tamaño</option>
-                <option value="s">S</option>
-                <option value="l">L</option>
-                <option value="m">M</option>
-              </select>
-              <select
-                value={materialFilter}
-                onChange={(e) => setMaterialFilter(e.target.value)}
-              >
-                <option value="">Material</option>
-                <option value="PLA">PLA</option>
-                <option value="ABS">ABS</option>
-                {/* Agregar más opciones de materiales según tu necesidad */}
-              </select>
-              <select
-                value={categoryFilter}
-                onChange={(e) => setCategoryFilter(e.target.value)}
-              >
-                <option value="">Categoría</option>
-                <option value="accesorio">accesorio</option>
-                <option value="Figuras">Figuras</option>
-                {/* Agregar más opciones de categorías según tu necesidad */}
-              </select>
-            </div>
-            <div className={style.FilterInputs}>
-              <input
-                type="number"
-                placeholder="Precio máximo"
-                value={priceFilter || ""}
-                onChange={(e) =>
-                  setPriceFilter(
-                    e.target.value === "" ? null : parseFloat(e.target.value)
-                  )
-                }
-              />
-              <select
-                value={sizeFilter || ""}
-                onChange={(e) =>
-                  setSizeFilter(e.target.value === "" ? null : e.target.value)
-                }
-              >
-                <option value="">Tamaño</option>
-                <option value="s">S</option>
-                <option value="l">L</option>
-                <option value="m">M</option>
-              </select>
-              <select
-                value={materialFilter}
-                onChange={(e) => setMaterialFilter(e.target.value)}
-              >
-                <option value="">Material</option>
-                <option value="PLA">PLA</option>
-                <option value="ABS">ABS</option>
-                {/* Agregar más opciones de materiales según tu necesidad */}
-              </select>
-              <select
-                value={categoryFilter}
-                onChange={(e) => setCategoryFilter(e.target.value)}
-              >
-                <option value="">Categoría</option>
-                <option value="accesorio">accesorio</option>
-                <option value="Figuras">Figuras</option>
-                {/* Agregar más opciones de categorías según tu necesidad */}
-              </select>
-            </div>
           </div>
           <div className={style.ContainerCards}>
             {loading ? (
@@ -223,7 +102,7 @@ function Home() {
             ) : error ? (
               <p>{error}</p>
             ) : (
-              filteredProducts?.map((e) => (
+              allProducts?.map((e) => (
                 <Card
                   key={e.id}
                   name={e.name}
