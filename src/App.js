5b8bdc3ffa877f8a44ff5013030e69b8c2ec10fc
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-10-27 10:38:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Route, Routes } from "react-router-dom";
  import Home from "./views/Home/Home.jsx";
 import { Login, Inventory } from "./views";
 import NavBar from "./Components/NavBar/NavBar.jsx";
+import Register from './views/Register/Register';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
-
-        <Route path="/Registrarse" element={<Login />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Register" element={<Register />}></Route>
         <Route path="/Inventario" element={<Inventory />}></Route>
       </Routes>
     </div>
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from "./login/Login.jsx";
 export { default as Inventory } from "./Inventory/Inventory.jsx";
 
+
