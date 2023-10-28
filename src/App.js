bf1e08634381675474ac73b5f94748ea6f3062db
Francisco Azvalinsky <frankyazva@gmail.com>
2023-10-28 20:38:55 -0300

    Intento de avanzar con la implementacion de paypal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
  import Home from "./views/Home/Home.jsx";
 import { Login, Inventory } from "./views";
 import NavBar from "./Components/NavBar/NavBar.jsx";
+import PagoPaypal from "./Components/PagoPaypal/PagoPaypal.jsx";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-
+        <Route path="/Pagar" element={<PagoPaypal />}></Route>
         <Route path="/Registrarse" element={<Login />}></Route>
         <Route path="/Inventario" element={<Inventory />}></Route>
       </Routes>
