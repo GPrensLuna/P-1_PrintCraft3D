6157944e670d177d69a12e1e06a746f123e2ecf2
GPrensLuna <prens.giova12@gmail.com>
2023-10-27 21:11:06 -0500

    :white_check_mark: INICIO :rocket:  :tada:

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,8 @@
 import { Container } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 import logo from "../../imagenes/logo.png";
 
@@ -26,24 +26,25 @@ function NavBar() {
           style={{ maxHeight: "100px" }}
           navbarScroll
         >
-          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/Registrarse">Login up</Nav.Link>
+          <Nav.Link href="/Inventario">Inventario</Nav.Link>
           <Nav.Link href="#action2">Link</Nav.Link>
         </Nav>
       </Container>
 
       <Container>
-      <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-light">Search</Button>
-          </Form>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-light">Search</Button>
+        </Form>
       </Container>
     </Navbar>
-   
   );
 }
 
