6c72a5ab500d0a5613c80d10a32c7f18261e3eb7
GPrensLuna <prens.giova12@gmail.com>
2023-10-30 13:36:32 -0500

    :recycle: :tada: :rocket:Actualizacion

--- src/views/LoyoutAdmin/ProductList.jsx
+++ src/views/LoyoutAdmin/ProductList.jsx
@@ -4,7 +4,8 @@ import styles from "./ProductList.module.css";
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
-  const [editingProduct, setEditingProduct] = useState(null);
+  const [editedFields, setEditedFields] = useState({});
+  const [editStatus, setEditStatus] = useState({});
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -15,6 +16,7 @@ export default function ProductList() {
             "Content-Type": "application/json",
           },
         });
+        console.log("Response from server:", response);
         const data = await response.json();
         setProducts(data);
       } catch (error) {
@@ -25,19 +27,49 @@ export default function ProductList() {
     fetchProducts();
   }, []);
 
-  const handleEdit = (productId) => {
-    setEditingProduct(productId);
+  const handleEdit = (productId, product) => {
+    setEditStatus((prevEditStatus) => ({
+      ...prevEditStatus,
+      [productId]: { editing: true },
+    }));
+    // Set initial values for edited fields based on the product
+    setEditedFields({
+      name: product.name,
+      image: product.image,
+      description: product.description,
+      price: product.price,
+      stock: product.stock,
+      material: product.material.name,
+      category: product.category.name,
+      deleted: product.deleted,
+    });
   };
 
-  const handleSave = async () => {
-    if (editingProduct === null) {
-      return;
-    }
+  const handleFieldChange = (fieldName, value) => {
+    setEditedFields((prevEditedFields) => ({
+      ...prevEditedFields,
+      [fieldName]: fieldName === "price" ? parseFloat(value) : value,
+    }));
+  };
+
+  const handleInlineUpdate = async (productId, e) => {
+    e.preventDefault(); // Evitar que el formulario se envíe por completo y recargue la página
 
     try {
-      const editedProduct = products.find((p) => p.id === editingProduct);
+      const productToEdit = products.find((p) => p.id === productId);
 
-      const response = await fetch(`${URL}ProductsLista/${editingProduct}`, {
+      if (!productToEdit) {
+        console.error("Product not found for editing.");
+        return;
+      }
+
+      const editedProduct = {
+        ...productToEdit,
+        ...editedFields,
+      };
+      console.log(editedProduct);
+
+      const response = await fetch(`${URL}ProductsLista/${productId}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -45,18 +77,50 @@ export default function ProductList() {
         body: JSON.stringify(editedProduct),
       });
 
-      if (!response.ok) {
-        console.error("Error updating product. Response:", response);
+      if (response.ok) {
+        const updatedProduct = await response.json();
+        setProducts((prevProducts) =>
+          prevProducts.map((p) => (p.id === productId ? updatedProduct : p))
+        );
+        setEditStatus((prevEditStatus) => ({
+          ...prevEditStatus,
+          [productId]: { editing: false },
+        }));
+        setEditedFields({});
+      } else {
+        const errorData = await response.json();
+        console.error("Error updating product. Server response:", errorData);
       }
-
-      setEditingProduct(null);
     } catch (error) {
       console.error("Error updating product:", error);
     }
   };
 
-  const handleCancelEdit = () => {
-    setEditingProduct(null);
+  const renderInputField = (fieldName, value, placeholder) => (
+    <div className={styles.inputContainer}>
+      <label className={styles.inputLabel}>{fieldName}:</label>
+      <input
+        className={styles.editInput}
+        type="text"
+        value={value !== undefined ? value : placeholder}
+        onChange={(e) => handleFieldChange(fieldName, e.target.value)}
+      />
+    </div>
+  );
+
+  const renderTableCell = (product, fieldName) => {
+    const isEditing = editStatus[product.id]?.editing;
+    const value = isEditing ? editedFields[fieldName] : product[fieldName];
+
+    return (
+      <td key={fieldName}>
+        {isEditing
+          ? renderInputField(fieldName, value, product[fieldName])
+          : fieldName === "material" || fieldName === "category"
+          ? value.name // Render only the 'name' property
+          : value}
+      </td>
+    );
   };
 
   return (
@@ -68,41 +132,46 @@ export default function ProductList() {
             <th>Name</th>
             <th>Image</th>
             <th>Description</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Material</th>
+            <th>Category</th>
+            <th>Deleted</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {products.map((product) => (
             <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.image}</td>
-              <td>
-                {editingProduct === product.id ? (
-                  <input
-                    type="text"
-                    value={product.description}
-                    onChange={(e) => {
-                      setProducts((prevProducts) =>
-                        prevProducts.map((p) =>
-                          p.id === product.id
-                            ? { ...p, description: e.target.value }
-                            : p
-                        )
-                      );
-                    }}
-                  />
-                ) : (
-                  product.description
-                )}
-              </td>
+              {renderTableCell(product, "name")}
+              {renderTableCell(product, "image")}
+              {renderTableCell(product, "description")}
+              {renderTableCell(product, "price")}
+              {renderTableCell(product, "stock")}
+              {renderTableCell(product, "material")}
+              {renderTableCell(product, "category")}
+              {renderTableCell(product, "deleted")}
               <td className={styles.actions}>
-                {editingProduct === product.id ? (
+                {editStatus[product.id]?.editing ? (
                   <>
-                    <button onClick={handleSave}>Save</button>
-                    <button onClick={handleCancelEdit}>Cancel</button>
+                    <button onClick={(e) => handleInlineUpdate(product.id, e)}>
+                      Save
+                    </button>
+                    <button
+                      onClick={() =>
+                        setEditStatus((prevEditStatus) => ({
+                          ...prevEditStatus,
+                          [product.id]: { editing: false },
+                        }))
+                      }
+                    >
+                      Cancel
+                    </button>
                   </>
                 ) : (
-                  <button onClick={() => handleEdit(product.id)}>Edit</button>
+                  <button onClick={() => handleEdit(product.id, product)}>
+                    Edit
+                  </button>
                 )}
               </td>
             </tr>
