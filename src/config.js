7e957d7b655d7f6110a36d528fd73522d0488e5b
GPrensLuna <prens.giova12@gmail.com>
2023-10-29 19:45:53 -0500

    :Bug: Registes con validacion de user

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const URL = "https://printcraft3d.up.railway.app/PrintCraft3D/";
 // const URL = "http://localhost:3001/PrintCraft3D/";
-=======
-//const URL = "https://printcraft3d.up.railway.app/PrintCraft3D/";
-const URL = "http://localhost:3001/PrintCraft3D/";
->>>>>>> 574c040d4cf2aba0ffd31036c9a84aa781d771e8
 
 
 
