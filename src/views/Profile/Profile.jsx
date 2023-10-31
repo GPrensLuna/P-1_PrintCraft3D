822095a79e6d89079a217c712655fc797b5eba39
Alan <sachero10@gmail.com>
2023-10-31 19:35:41 -0300

    5

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -4,11 +4,6 @@ export default function Profile({ userData }) {
       <br />
       <br />
       <br />
-      <br />
-      <h1>Componente de Perfil</h1>
-      <br />
-      <br />
-      <br />
       {userData ? (
         <div>
           <h1>Hola {userData.email}, bienvenido a tu perfil</h1>
