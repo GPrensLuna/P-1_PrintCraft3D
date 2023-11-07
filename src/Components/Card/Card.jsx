7b6c6f89886a5fc245ad85b828cdaf119a15cefe
LuisReyesP <relujo@hotmail.com>
2023-11-07 00:16:20 -0500

    anadiendo card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from "./Card.module.css";
+<<<<<<< Updated upstream
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+=======
+import "./Card.css";
+>>>>>>> Stashed changes
 
 const Card = (props) => {
   const {
@@ -33,6 +37,7 @@ const Card = (props) => {
     const idProduct = id;
     onDelete(idProduct);
   };
+<<<<<<< Updated upstream
   const userData = useSelector((state) => state.userData);
   const DeleteButton = () => {
     if (!userData || userData.roll === null) {
@@ -74,6 +79,49 @@ const Card = (props) => {
         <p className={style.price}>Precio: {priceFormatted}</p>
         <p className={style.material}>Material: {materialName}</p>
         <p className={style.category}>Categoría: {categoryName}</p>
+=======
+  return (
+    // <div className={style.Card}>
+    //   <button className={style.onClonse} onClick={handleDelete}>
+    //     X
+    //   </button>
+    //   <img className={style.Imagen} src={image} alt={name} />
+    //   <div className={style.Container}>
+    //     <h3 className={style.name}>{nameM}</h3>
+    //     <p className={style.description}>{description}</p>
+    //     <p className={style.size}>Tamaño: {sizeM}</p>
+    //     <p className={style.price}>Precio: {priceFormatted}</p>
+    //     <p className={style.Material}>Material: {materialName}</p>
+    //     <p className={style.Category}>Categoría: {categoryName}</p>
+    //   </div>
+    // </div>
+    <div className="product-card">
+      <img className="product-image" src={image} />{" "}
+      <div className="size-info">
+        {" "}
+        <strong>size: {size}</strong>
+      </div>
+      <div className="product-title">
+        <strong>{name}</strong>
+      </div>
+      <div className="product-desc">{description}</div>
+      <div className="cart-price">
+        <span className="offer-price">
+          <strong>${price}</strong>
+        </span>
+      </div>
+      <div className="add-to-cart-parent">
+        <div className="cart-parent">
+          <div className="ml-auto"></div>
+          <div className="material-info">
+            <strong>▓ {Material}</strong>
+          </div>
+          <button className="cart-btn">
+            <i className="fa fa-plus"></i>
+            <strong>Add to Cart</strong>
+          </button>
+        </div>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
+<<<<<<< Updated upstream
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history"; // Import createBrowserHistory
 import reportWebVitals from "./reportWebVitals";
+=======
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import App from "./App";
+>>>>>>> Stashed changes
 import store from "./redux/store";
 import "./App.css";
 import App from "./App";
 
+<<<<<<< Updated upstream
 // Create a history object
 const history = createBrowserHistory();
 
 // Use the history object in the Router
+=======
+>>>>>>> Stashed changes
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
