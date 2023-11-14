4e6012b0bc7fd7bd2f7c464975406caed41858fa
GPrensLuna <prens.giova12@gmail.com>
2023-11-13 20:24:34 -0500

    :recycle: Actualizacion

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
- //const URL = "https://printcraft3d.up.railway.app/PrintCraft3D/";
+ const URL = "https://printcraft3d.up.railway.app/PrintCraft3D/";
  const URLCLOUD = "https://api.cloudinary.com/v1_1/deeufsn3k/image/upload";
-const URL = "http://localhost:3001/PrintCraft3D/";
+//const URL = "http://localhost:3001/PrintCraft3D/";
 const CLIENTID= "1009242410508-qrbh4egjfjknhavfckvhock1o0trgt82.apps.googleusercontent.com"
 
 
