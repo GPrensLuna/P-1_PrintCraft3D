b63d2ee39bbe60918f2c55d208728643f998d801
GPrensLuna <prens.giova12@gmail.com>
2023-11-13 22:31:50 -0500

    :recycle: Actualizacion

--- src/Components/LoginRedSocial/LoginRedSocial.jsx
+++ src/Components/LoginRedSocial/LoginRedSocial.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import google from "../../imagenes/google.png";
 import axios from "axios";
 import { URL } from '../../config.js';
 import { GoogleAuthProvider, signInWithPopup, auth } from "../../firebase.js";
+import { LoginUser } from "../../redux/actions/actions.js";
+import {  useDispatch } from "react-redux";
 
 const LoginRedSocial = () => {
+  const dispatch = useDispatch();
 
+  const [loading, setLoading] = useState(false);
+  const [userRegistered, setUserRegistered] = useState(false);
+  const [userData, setUserData] = useState(null);
   const handleGoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
   
     try {
+      setLoading(true);
+  
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
   
@@ -23,12 +31,31 @@ const LoginRedSocial = () => {
   
       const receivedToken = response.data.token;
       localStorage.setItem("token", receivedToken);
+  
+      // Actualizar el estado si el usuario ya está registrado
+      setUserRegistered(true);
+      dispatch(LoginUser({
+        firstName: user.displayName,
+        email: user.email,
+        roll: "Client",
+      }));
+      setUserData({
+        firstName: user.displayName,
+        email: user.email,
+        roll: "Client",
+      });
+  
       window.location.href = "/Profile";
+
+  
     } catch (error) {
       console.error("Error al autenticar con Google:", error.message);
       console.error("Detalles del error:", error.response);
+    } finally {
+      setLoading(false);
     }
   };
+  
 
   return (
     <div className="container w-100 my-4">
@@ -41,6 +68,7 @@ const LoginRedSocial = () => {
             type="button"
             className="btn btn-outline-danger w-100 my-1"
             onClick={handleGoogleSignIn}
+            disabled={userRegistered} 
           >
             <div className="row align-items-center">
               <div className="col-2">
@@ -51,6 +79,17 @@ const LoginRedSocial = () => {
           </button>
         </div>
       </div>
+      {loading && (
+        <div className="row text-center mt-2">
+          <div className="col-12">Cargando...</div>
+        </div>
+      )}
+      {userRegistered && userData && (
+        <div className="row text-center mt-2">
+          <div className="col-12">
+          </div>
+        </div>
+      )}
     </div>
   );
 };
