9e0bc8ff2ecadf97a414c477306ea8c61c1b5bf4
LuisReyesP <relujo@hotmail.com>
2023-11-16 15:48:47 -0500

    sincronizado el ultimo commit para empezar a trabajar en el paginado (otra vez)

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CardCart from "../CardCart/CardCart.jsx";
 import PagoPaypal from "../PagoPaypal/PagoPaypal.jsx";
+//import style from "./ShoppingCart.module.css";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { URL } from "../../config.js";
