acdc928235d8bddd912b31b6a5a171dbbb85dd86
LuisReyesP <relujo@hotmail.com>
2023-11-16 17:12:24 -0500

    estilizado del paginado, NaN sin solucionar todavia

--- src/Components/Paginado/Paginado.jsx
+++ src/Components/Paginado/Paginado.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "./Paginado.css";
 
-const Paginado = () => {
-  return <h1>paginado</h1>;
+const Paginado = ({ currentPage, totalPages, handleLimitChange, loadPage }) => {
+  return (
+    <div>
+      <button
+        className="anterior-button Pbtn"
+        onClick={() => loadPage(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Anterior
+      </button>
+      <span>
+        {currentPage} de {totalPages}
+      </span>
+      <button
+        className="siguiente-button Pbtn"
+        onClick={() => loadPage(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Siguiente
+      </button>
+      <hr></hr>
+
+      <p>Items por pagina: </p>
+      <select onChange={handleLimitChange} id="limit" defaultValue={12}>
+        <option value="4">4</option>
+        <option value="8">8</option>
+        <option value="12">12</option>
+        <option value="24">24</option>
+        <option value="48">48</option>
+      </select>
+    </div>
+  );
 };
 
 export default Paginado;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -9,10 +9,8 @@ import style from "./Home.module.css";
 import Reviews from "../../Components/Review/Review.jsx";
 import Card from "../../Components/Card/Card.jsx";
 import CarouselHome from "../../Components/CarouselHome/CarouselHome.jsx";
-import {
-  addProductInfo,
-  addFilteredProductList,
-} from "../../redux/actions/actions.js";
+import Paginado from "../../Components/Paginado/Paginado.jsx";
+import { addProductInfo } from "../../redux/actions/actions.js";
 
 import Swal from "sweetalert2";
 
@@ -127,8 +125,6 @@ function Home() {
     dispatch,
   ]);
 
-  const filteredProducts = useSelector((state) => state.filteredProductsList);
-
   useEffect(() => {
     localStorage.setItem("currentPage", currentPage);
   }, [currentPage]);
@@ -173,22 +169,20 @@ function Home() {
         userId,
         productId,
       });
-      const { message } = response.data
-      if (response.status===201){
+      const { message } = response.data;
+      if (response.status === 201) {
+        let cart = JSON.parse(localStorage.getItem("cart"));
 
-        let cart = JSON.parse(localStorage.getItem("cart"))
+        let index = cart.findIndex((product) => product.id === productId);
 
-        let index = cart.findIndex ((product) => product.id === productId)
-
-        if (cart[index].cantidad-1===0){
+        if (cart[index].cantidad - 1 === 0) {
           cart.splice(index, 1);
-        }
-        else {
-          cart[index].cantidad-=1
+        } else {
+          cart[index].cantidad -= 1;
         }
 
-        localStorage.setItem("cart", JSON.stringify(cart))
-  
+        localStorage.setItem("cart", JSON.stringify(cart));
+
         Swal.fire({
           position: "center",
           icon: "error",
@@ -211,7 +205,7 @@ function Home() {
   };
 
   const handleProductAddToCart = (productId) => {
-    if (userData?.userId){
+    if (userData?.userId) {
       const currentCart = JSON.parse(localStorage.getItem("cart")) || [];
       //Busca si existe el id del producto en el carrito
       const existingProductIndex = currentCart.findIndex(
@@ -224,23 +218,25 @@ function Home() {
         );
         if (productToAdd) {
           //Una vez que lo encuentra en el Estado lo agrega al carrito
-            const updatedCart = [...currentCart, { ...productToAdd, cantidad: 1 }];
-            localStorage.setItem("cart", JSON.stringify(updatedCart));
+          const updatedCart = [
+            ...currentCart,
+            { ...productToAdd, cantidad: 1 },
+          ];
+          localStorage.setItem("cart", JSON.stringify(updatedCart));
         }
       } else {
         // Si ya está en el carrito, actualiza el contador
-          const updatedCart = [...currentCart];
-          updatedCart[existingProductIndex] = {
-            ...updatedCart[existingProductIndex],
-            cantidad: updatedCart[existingProductIndex].cantidad + 1,
-          };
-          localStorage.setItem("cart", JSON.stringify(updatedCart));
+        const updatedCart = [...currentCart];
+        updatedCart[existingProductIndex] = {
+          ...updatedCart[existingProductIndex],
+          cantidad: updatedCart[existingProductIndex].cantidad + 1,
+        };
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
       }
       //console.log(`User ID: ${userData.userId}`);
       //console.log(`Product ID: ${productId}`);
       addToCart(userData.userId, productId);
-    }
-    else {
+    } else {
       Swal.fire({
         position: "center",
         icon: "error",
@@ -250,7 +246,6 @@ function Home() {
         timer: 2000,
       });
     }
-
   };
 
   return (
@@ -297,32 +292,17 @@ function Home() {
           )}
         </div>
       </div>
-      <div className={style.ContainerFilter}>
-        <button
-          className={style.BTNPreviu}
-          onClick={() => loadPage(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Anterior
-        </button>
-        <span className={style.SpanCurrentPage}>
-          {currentPage} de {totalPages}
-        </span>
-        <button
-          className={style.BTNNext}
-          onClick={() => loadPage(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          Siguiente
-        </button>
-        <select onChange={handleLimitChange} id="limit" defaultValue={12}>
-          <option value="4">4</option>
-          <option value="8">8</option>
-          <option value="12">12</option>
-          <option value="24">24</option>
-          <option value="48">48</option>
-        </select>
+      <div className={style.paginationWrapper}>
+        <div>
+          <Paginado
+            currentPage={currentPage}
+            totalPages={totalPages}
+            handleLimitChange={handleLimitChange}
+            loadPage={loadPage}
+          />
+        </div>
       </div>
+
       <div className={style.ContainerFooter}>
         <Footer />
       </div>
