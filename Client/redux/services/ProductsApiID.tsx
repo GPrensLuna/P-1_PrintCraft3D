c09201c81cd288b80cb74fe67cb07bd822534860
GPrensLuna <Prens.giova12@gmail.com>
2024-01-19 14:40:24 -0500

    diseño terminado revisar errores de babel, implemetar login de terceros y register

--- Client/redux/services/ProductsApiID.tsx
+++ Client/redux/services/ProductsApiID.tsx
@@ -11,25 +11,9 @@ export const ProductsApiID = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: URL_BACKEND }),
     endpoints: (builder) => ({
         getProducts: builder.query<Product[], LoadProductsArgs>({
-            query: async ({ id }) => {
-                try {
-                    const url = `Product/${id}`;
-                    console.log(url);
-                    const response = await fetch(url);
-
-                    if (!response.ok) {
-                        throw new Error("Request failed");
-                    }
-
-                    const data = await response.json();
-                    return data;
-                } catch (error) {
-                    throw new Error(`Error fetching products: ${error.message}`);
-                }
-            },
+            query: ({ id }) => `Product/${id}`, // Devolvemos la URL directamente
         }),
     }),
 });
-
 
 export const { useGetProductsQuery } = ProductsApiID;
