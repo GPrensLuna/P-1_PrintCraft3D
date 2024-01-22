f5caee18fe857f297998e6030a78c937fff88946
GPrensLuna <Prens.giova12@gmail.com>
2024-01-22 08:35:08 -0500

    correciones

--- Server/src/config.js
+++ Server/src/config.js
@@ -1,8 +1,8 @@
 const PORT = process.env.PORT;
 const LOCALHOST = process.env.LOCALHOST;
 
-// /*  const PORT = 3001;
-//  const LOCALHOST = "localhost";
+//const PORT = 3001;
+//const LOCALHOST = "localhost";
 
 const URL_FONT = "https://p-1-print-craft3d.vercel.app/";
 const SECRETKEY = "printcraft3d";
@@ -12,12 +12,13 @@ const NODE_ENV = process.env.NODE_ENV || true;
 const clientID =
   "1009242410508-qrbh4egjfjknhavfckvhock1o0trgt82.apps.googleusercontent.com";
 
-const DB_USER = process.env.DB_USER || "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD || "2728";
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_NAME = process.env.DB_NAME || "printcraft3d";
-const DB_PORT = process.env.DB_PORT || 5432;
-const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+//const DB_USER = process.env.DB_USER || "postgres";
+//const DB_PASSWORD = process.env.DB_PASSWORD || "2728";
+//const DB_HOST = process.env.DB_HOST || "localhost";
+//const DB_NAME = process.env.DB_NAME || "printcraft3d";
+//const DB_PORT = process.env.DB_PORT || 5432;
+const DB_URL = process.env.DB_URL;
+//const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 const ENVIRONMENT = process.env.ENVIRONMENT;
 const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID;
 const PAYPAL_CLIENT_SECRET = process.env.PAYPAL_CLIENT_SECRET;
