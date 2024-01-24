673dc28533b9682c28066341895bca0221bc7b60
GPrensLuna <Prens.giova12@gmail.com>
2024-01-24 17:56:58 -0500

    login tercero

--- Client/Components/LogIn/LoginRedSocial.tsx
+++ Client/Components/LogIn/LoginRedSocial.tsx
@@ -1,104 +1,24 @@
 "use client"
-import { useState } from 'react';
 import Image from 'next/image';
-import { useDispatch } from 'react-redux';
-import { useRouter } from 'next/navigation';
-import { GoogleAuthProvider, signInWithPopup, auth } from '@/firebase.js';
-import { setLoginUser } from '@/redux/features/LogInSlice';
+//import { useDispatch } from 'react-redux';
 import google from '@/img/google.webp';
-import { URL_BACKEND } from '@/config';
-import { UserState } from '@/Ts/Login'
+//import { URL_BACKEND } from '@/config';
+import { signIn, useSession } from "next-auth/react";
+//import { useRouter } from 'next/navigation';
 
 export const LoginRedSocial = () => {
-  const dispatch = useDispatch();
-  const router = useRouter();
 
-  const [userRegistered, setUserRegistered] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [userData, setUserData] = useState({});
+  //const { data: session } = useSession();
 
-  const handleGoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
+  //console.log(session)
 
-    try {
-      setLoading(true);
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-
-      const response = await fetch(`${URL_BACKEND}/Google`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          firstName: user.displayName,
-          email: user.email,
-          roll: "Client",
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error en la solicitud: ${response.status}`);
-      }
-
-      const responseData = await response.json();
-
-      const receivedToken = responseData.token;
-      const id = responseData.id;
-
-      localStorage.setItem('token', receivedToken);
-      localStorage.setItem('userData', JSON.stringify({
-        firstName: user.displayName,
-        email: user.email,
-        roll: "Client",
-        userId: id,
-      }));
-
-      setUserRegistered(true);
-      dispatch(setLoginUser({
-        firstName: user.displayName,
-        email: user.email,
-        roll: "Client",
-      }));
-      setUserData({
-        ...userData,
-        userId: id,
-      });
-
-      if (id) {
-        createCart(id);
-      }
-
-      router.push('/Profile');  // Redirección usando Next.js router
-    } catch (error) {
-      console.error('Error al autenticar con Google:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const createCart = async (userId: number) => {
-    try {
-      const response = await fetch(`${URL_BACKEND}/shoppingCart`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ userId }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error en la solicitud: ${response.status}`);
-      }
-
-      const responseData = await response.json();
-      console.log('Respuesta de la creación del carrito:', responseData);
-    } catch (error) {
-      console.error('Error al crear el carrito:', error);
-    }
-  };
 
   return (
     <div className="container w-100 my-4">
       <div className="row">
         <div className="col">
-          <button onClick={handleGoogleSignIn} type="button" className="btn btn-outline-danger w-100 my-1" disabled={userRegistered}>
+          <button onClick={() => signIn('google')} type="button" className="btn btn-outline-danger w-100 my-1"
+          >
             <div className="row align-items-center">
               <div className="col-2">
                 <Image src={google} alt="Google" width="32" />
@@ -109,11 +29,6 @@ export const LoginRedSocial = () => {
         </div>
       </div>
 
-      {loading && (
-        <div className="row text-center mt-2">
-          <div className="col-12">Cargando...</div>
-        </div>
-      )}
     </div>
   );
 };
