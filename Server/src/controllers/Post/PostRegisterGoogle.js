8f96197692f5eb8498f58cbfa09549029a980a35
GPrensLuna <Prens.giova12@gmail.com>
2024-02-13 12:24:39 -0500

    correccion

--- Server/src/controllers/Post/PostRegisterGoogle.js
+++ Server/src/controllers/Post/PostRegisterGoogle.js
@@ -10,6 +10,8 @@ async function PostRegisterGoogle(req, res) {
       where: { email: req.body.email },
     });
 
+    console.log("existingUser", existingUser);
+
     if (existingUser) {
       const token = jwt.sign(
         {
