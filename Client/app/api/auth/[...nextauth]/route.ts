e313126e8b1cdf62098ebb54f58316c4a9b967f0
GPrensLuna <Prens.giova12@gmail.com>
2024-02-13 17:26:29 -0500

    configuracion

--- Client/app/api/auth/[...nextauth]/route.ts
+++ Client/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { NextApiHandler } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
@@ -39,78 +39,82 @@ interface OAuthConfig {
   authorize: (credentials: any, req: any) => Promise<any>;
 }
 
-const handler: NextApiHandler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "email", type: "email", placeholder: "test@test.com" },
-        password: { label: "Password", type: "password" },
+const nextAuthHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const handler = NextAuth(req, res, {
+    providers: [
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: { label: "email", type: "email", placeholder: "test@test.com" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials, req) {
+          const res = await fetch(`${URL_BACKEND}login`, {
+            method: "POST",
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+            headers: { "Content-Type": "application/json" },
+          });
+          const user = await res.json();
+
+          if (user.error) throw new Error(user.error);
+          return user;
+        },
+      }),
+      GoogleProvider({
+        clientId: GOOGLE_CLIENT_ID as string,
+        clientSecret: GOOGLE_CLIENT_SECRET as string,
+        authorize: googleAuthorize,
+      } as OAuthConfig),
+    ],
+    callbacks: {
+      async jwt({ token, user, account }) {
+        if (account && user) {
+          const expiryTime = Date.now() + 1 * 60 * 60 * 1000;
+          token.expiryTime = expiryTime;
+          token.roll = user.roll;
+        }
+        return token;
       },
-      async authorize(credentials, req) {
-        const res = await fetch(`${URL_BACKEND}login`, {
-          method: "POST",
-          body: JSON.stringify({
-            email: credentials?.email,
-            password: credentials?.password,
-          }),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
 
-        if (user.error) throw new Error(user.error);
-        return user;
+      async session({ session, token }) {
+        if (
+          typeof token.expiryTime === "number" &&
+          Date.now() > token.expiryTime
+        ) {
+          return {
+            ...session,
+            expires: "1970-01-01T00:00:00Z",
+            user: { ...session.user, id: "", name: "", email: "", image: "" },
+          };
+        }
+
+        if (token.roll) {
+          session.user = {
+            ...session.user,
+            roll: typeof token.roll === "string" ? token.roll : undefined,
+          };
+        }
+        return session;
       },
-    }),
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID as string,
-      clientSecret: GOOGLE_CLIENT_SECRET as string,
-      authorize: googleAuthorize,
-    } as OAuthConfig),
-  ],
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        const expiryTime = Date.now() + 1 * 60 * 60 * 1000;
-        token.expiryTime = expiryTime;
-        token.roll = user.roll;
-      }
-      return token;
+      async redirect({ url, baseUrl }) {
+        if (
+          url.startsWith(baseUrl + "/LoginUp") ||
+          url.startsWith(baseUrl + "/api/auth/signout")
+        ) {
+          return baseUrl + "/Profile";
+        }
+        return url.startsWith(baseUrl) ? url : baseUrl;
+      },
     },
-
-    async session({ session, token }) {
-      if (
-        typeof token.expiryTime === "number" &&
-        Date.now() > token.expiryTime
-      ) {
-        return {
-          ...session,
-          expires: "1970-01-01T00:00:00Z",
-          user: { ...session.user, id: "", name: "", email: "", image: "" },
-        };
-      }
-
-      if (token.roll) {
-        session.user = {
-          ...session.user,
-          roll: typeof token.roll === "string" ? token.roll : undefined,
-        };
-      }
-      return session;
+    pages: {
+      signIn: "/LoginUp",
     },
-    async redirect({ url, baseUrl }) {
-      if (
-        url.startsWith(baseUrl + "/LoginUp") ||
-        url.startsWith(baseUrl + "/api/auth/signout")
-      ) {
-        return baseUrl + "/Profile";
-      }
-      return url.startsWith(baseUrl) ? url : baseUrl;
-    },
-  },
-  pages: {
-    signIn: "/LoginUp",
-  },
-});
+  });
 
-export { handler as GET, handler as POST };
+  await handler(req, res);
+};
+
+export default nextAuthHandler;
--- Client/config.js
+++ Client/config.js
@@ -1,11 +1,6 @@
 export const URL_BACKEND = process.env.NEXT_PUBLIC_URL_BACKEND;
-
 export const SECRET = process.env.NEXTAUTH_SECRET;
-
 export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
 export const NEXT_PUBLIC_PAYPAL = process.env.NEXT_PUBLIC_PAYPAL;
-
 export const URL_CLOUDINARY = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
